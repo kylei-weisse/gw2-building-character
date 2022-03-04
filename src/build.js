e8199933b70cb21422d032794ef68f8d511e67dc
Kyle Weisse <kyle.weisse@gmail.com>
2022-03-04 17:28:01 -0500

    added test words for build component

--- src/build.js
+++ src/build.js
@@ -1,4 +1,18 @@
 import './App.css';
 import React from 'react'; //do I need to use this everywhere?
 
-comp
+class Build extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render(){
+        return(
+            <h1>succesfully made it to the build component!</h1>
+        )
+    }
+}
