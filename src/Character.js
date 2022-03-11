32f46dc39633160331c18cafcdb5cfa358568008
Kyle Weisse <kyle.weisse@gmail.com>
2022-03-11 16:02:41 -0500

    hooks

--- src/Character.js
+++ src/Character.js
@@ -1,15 +1,36 @@
 import './App.css';
-import React from 'react'; //do I need to use this everywhere?
+import React, { useState } from 'react'; //do I need to use this everywhere?
 import { useParams } from "react-router-dom";
+
+import { token } from './Api'
 
 
 export default function Character() {
-    let params = useParams();
+    
+    const[character, setCharacter] = useState(null)
+
+    let params = useParams()
+
+    if (!character){
+        fetch(
+        "https://api.guildwars2.com/v2/characters/" + params.characterID + "?access_token=" + token)
+        .then((response) => response.json())
+        .then((json) => {
+            this.setState({
+                names: json,
+                dataIsLoaded: true
+            });
+        })
+    }
+
+
+
     return (
         <>
             <h1>{params.characterID}</h1>
         </>
     )
+
 
 }
 
