f277b6e1a74d230d831773dfdf9de8f5b5f079df
Kyle Weisse <kyle.weisse@gmail.com>
2022-03-11 11:42:15 -0500

    added equipment function

--- src/Character.js
+++ src/Character.js
@@ -2,6 +2,11 @@ import './App.css';
 import React from 'react'; //do I need to use this everywhere?
 import { useParams } from "react-router-dom";
 
+
+function Equipment(token){
+    return ("https://api.guildwars2.com/v2/mounts?access_token="+token)
+}
+
 export default function Character(props){
     let params = useParams();
         return(
