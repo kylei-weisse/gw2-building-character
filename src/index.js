69637d98890ed8c7575bf050cccc8a06c2db2b68
Kyle Weisse <kyle.weisse@gmail.com>
2022-03-11 11:41:55 -0500

    removed some boilerplate

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import App from './App';
 import CharacterList from './CharacterList';
 import Character from './Character';
 
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -30,7 +29,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+
