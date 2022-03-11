235c72067032b23ef200e28c88d49c9aa6d9aa18
Kyle Weisse <kyle.weisse@gmail.com>
2022-03-11 14:43:12 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 export default function App() {
     return (
     <div>
-       <h1>Character Manager</h1> {/*TODO: put some styling here */}
+       <h1 className="banner">Character Manager</h1> {/*TODO: put some styling here */}
        <Link to="/characterlist">Character List</Link>
 
        <Outlet />
