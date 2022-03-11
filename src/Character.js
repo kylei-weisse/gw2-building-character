dcb5f3b66376502756d93d65df383c5fe5bc9231
Kyle Weisse <kyle.weisse@gmail.com>
2022-03-11 14:43:05 -0500

    took equipment component into new file

--- src/Character.js
+++ src/Character.js
@@ -3,15 +3,13 @@ import React from 'react'; //do I need to use this everywhere?
 import { useParams } from "react-router-dom";
 
 
-function Equipment(token){
-    return ("https://api.guildwars2.com/v2/mounts?access_token="+token)
-}
-
-export default function Character(props){
+export default function Character() {
     let params = useParams();
-        return(
-            <h1>This is {params.characterID}'s page</h1>
-        )
+    return (
+        <>
+            <h1>{params.characterID}</h1>
+        </>
+    )
 
 }
 
