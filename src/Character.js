f241d922de954e3409474b8bc9bdd9e31494d48e
Kyle Weisse <kyle.weisse@gmail.com>
2022-03-11 20:04:53 -0500

    added backlog

--- src/Character.js
+++ src/Character.js
@@ -23,12 +23,19 @@ export default function Character() {
         })
     }
 
-    
-    let helmAquatic = "jkl"
+    //all equipment. Note that this is stats not skins.
+    let helmAquatic, backpack, coat, boots, gloves = null
+
+
+    //TODO: put this in the promise chain
     if (character) {
         console.log(character.character.equipment)
         
         helmAquatic = character.character.equipment[0].id.toString()
+        backpack = character.character.equipment[1].id.toString()
+        coat = character.character.equipment[2].id.toString()
+        boots = character.character.equipment[3].id.toString()
+        gloves = character.character.equipment[4].id.toString()
     }
     else {console.log("loading")}
 
@@ -37,6 +44,10 @@ export default function Character() {
         <>
             <h1>{params.characterID}</h1>
             <h5>{helmAquatic}</h5>
+            <h5>{backpack}</h5>
+            <h5>{coat}</h5>
+            <h5>{boots}</h5>
+            <h5>{gloves}</h5>
         </>
     )
 
