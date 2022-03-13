4f33ef105ff1d795829909b5c86a88c73aff3979
Kyle Weisse <kyle.weisse@gmail.com>
2022-03-13 09:25:26 -0400

    added State hook to Equipment

--- src/Character.js
+++ src/Character.js
@@ -6,41 +6,45 @@ import { token } from './Api'
 
 
 export default function Character() {
-    const [character, setCharacter] = useState(null) 
+    const [character, setCharacter] = useState(null)
     const params = useParams()
 
     if (!character) {
-        fetch(
-            "https://api.guildwars2.com/v2/characters/" + params.characterID + "?access_token=" + token
-        )
+        fetch("https://api.guildwars2.com/v2/characters/" + params.characterID + "?access_token=" + token)
             .then(response => response.json())
             .then(json => setCharacter(json))
 
         return (<div>Loading ... </div>)
     }
 
-    return ( 
+    return (
         <>
-            <h1> hello {params.characterID}! </h1> 
+            <h1> hello {params.characterID}! </h1>
             {
-            character.equipment.map(
-                (item, index) => (
-                    //start jsx harmony using "<"
-                    <div key={`${index}:${item.id}`}> 
-                        <Equipment item={item}/>
-                    </div>
-                )
+                character.equipment.map(
+                    (item, index) => (
+                        //start jsx harmony using "<"
+                        <div key={`${index}:${item.id}`}>
+                            <Equipment item={item} />
+                        </div>
+                    )
                 )
             }
         </>
     )
 }
 
-function Equipment({item}) {
-    console.log(item)
-    var viewer =item.id
-    if (item.stats.attributes.power){
-        viewer+="\npower: "+item.stats.attributes.power
-    } 
+function Equipment({ item }) {
+
+    var viewer = item.id
+    const [thisItem, setThisItem] = useState([])
+
+    fetch("https://api.guildwars2.com/v2/items/" + item.id)
+        .then(response => response.json())
+        .then(json => setThisItem(json))
+     
+    console.log(thisItem)    
+
+    //todo: put all the item stats into an array and display the array, rather than making a string method.
     return (<div>{viewer}</div>)
 }
