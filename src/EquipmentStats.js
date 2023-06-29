bff2bae52fab7485d1c4513d53f0fd0caa22bd1b
Kylei Weisse <kylei.weisse@gmail.com>
2023-06-29 05:00:33 -0400

    added itemStat*stat* to EquipmentStat, trying to make it so that characterStat can be populated

--- src/EquipmentStats.js
+++ src/EquipmentStats.js
@@ -2,9 +2,15 @@ import { useParams } from "react-router-dom";
 //todo important: use this api call to get the stats of equipment.
 //I'm not sure if putting the stats in a array is the right call.
 
-var statPower=0
-let statConditionDamage=0
-
+let itemStatPower           =0
+let itemStatConditionDamage =0
+let itemStatToughness       =0
+let itemStatVitality        =0
+let itemStatCritRate        =0
+let itemStatCritDamage      =0
+let itemStatHealingPower    =0
+let itemStatBoonTime        =0
+let itemStatConditionTime   =0
 
 export default function EquipmentStats() {
     let params = useParams();
