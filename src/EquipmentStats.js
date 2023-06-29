2cc2b715d844b0241e6fa1e7432dba2d43af89e1
Kylei Weisse <kylei.weisse@gmail.com>
2023-06-29 05:06:07 -0400

    changed equipment stat names to match in game names, and made the creation order of the stats line up with the game itself

--- src/EquipmentStats.js
+++ src/EquipmentStats.js
@@ -3,14 +3,14 @@ import { useParams } from "react-router-dom";
 //I'm not sure if putting the stats in a array is the right call.
 
 let itemStatPower           =0
-let itemStatConditionDamage =0
+let itemStatPrecision       =0
 let itemStatToughness       =0
 let itemStatVitality        =0
-let itemStatCritRate        =0
-let itemStatCritDamage      =0
+let itemStatFerocity        =0
+let itemStatConditionDamage =0
 let itemStatHealingPower    =0
-let itemStatBoonTime        =0
-let itemStatConditionTime   =0
+let itemStatConcentration   =0
+let itemStatExpertise       =0
 
 export default function EquipmentStats() {
     let params = useParams();
